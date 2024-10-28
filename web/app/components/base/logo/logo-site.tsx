68186e64a168998616c56e60eb1c1fd27099b2ab
Ted <2508067350@qq.com>
2024-10-28 17:01:33 +0800

    Modify ui
    Modify ui

--- web/app/components/base/logo/logo-site.tsx
+++ web/app/components/base/logo/logo-site.tsx
@@ -16,7 +16,8 @@ const LogoSite: FC<LogoSiteProps> = ({
     }
   })
 
-  const src = theme === 'light' ? '/logo/logo-site.png' : `/logo/logo-site-${theme}.png`
+  const src = 'https://static.wixstatic.com/media/4741be_2f062f6c8ef74708b64652131fad6f07~mv2.png/v1/fill/w_30,h_34,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Frame%201597883169.png'
+
   return (
     <img
       src={src}
